ef7d8d8fb45ef65e7bdbbee6ae5165adb7491af8
wangshub <rocksong.hit@gmail.com>
2019-03-28 15:18:34 +0800

    feat: window management

---
+++ modules/clipboard.lua
@@ -0,0 +1,130 @@
+--[[
+   From https://github.com/victorso/.hammerspoon/blob/master/tools/clipboard.lua
+   Modified by Diego Zamboni
+
+   This is my attempt to implement a jumpcut replacement in Lua/Hammerspoon.
+   It monitors the clipboard/pasteboard for changes, and stores the strings you copy to the transfer area.
+   You can access this history on the menu (Unicode scissors icon).
+   Clicking on any item will add it to your transfer area.
+   If you open the menu while pressing option/alt, you will enter the Direct Paste Mode. This means that the selected item will be
+   "typed" instead of copied to the active clipboard.
+   The clipboard persists across launches.
+   -> Ng irc suggestion: hs.settings.set("jumpCutReplacementHistory", clipboard_history)
+]]--
+
+-- Feel free to change those settings
+local frequency = 0.8 -- Speed in seconds to check for clipboard changes. If you check too frequently, you will loose performance, if you check sparsely you will loose copies
+local hist_size = 100 -- How many items to keep on history
+local label_length = 70 -- How wide (in characters) the dropdown menu should be. Copies larger than this will have their label truncated and end with "…" (unicode for elipsis ...)
+local honor_clearcontent = false --asmagill request. If any application clears the pasteboard, we also remove it from the history https://groups.google.com/d/msg/hammerspoon/skEeypZHOmM/Tg8QnEj_N68J
+local pasteOnSelect = false -- Auto-type on click
+
+-- Don't change anything bellow this line
+local jumpcut = hs.menubar.new()
+jumpcut:setTooltip("Clipboard history")
+local pasteboard = require("hs.pasteboard") -- http://www.hammerspoon.org/docs/hs.pasteboard.html
+local settings = require("hs.settings") -- http://www.hammerspoon.org/docs/hs.settings.html
+local last_change = pasteboard.changeCount() -- displays how many times the pasteboard owner has changed // Indicates a new copy has been made
+
+--Array to store the clipboard history
+local clipboard_history = settings.get("so.victor.hs.jumpcut") or {} --If no history is saved on the system, create an empty history
+
+-- Append a history counter to the menu
+function setTitle()
+   if (#clipboard_history == 0) then
+      jumpcut:setTitle("✂") -- Unicode magic
+   else
+      jumpcut:setTitle("✂") -- Unicode magic
+      --      jumpcut:setTitle("✂ ("..#clipboard_history..")") -- updates the menu counter
+   end
+end
+
+function putOnPaste(string,key)
+   if (pasteOnSelect) then
+      hs.eventtap.keyStrokes(string)
+      pasteboard.setContents(string)
+      last_change = pasteboard.changeCount()
+   else
+      if (key.alt == true) then -- If the option/alt key is active when clicking on the menu, perform a "direct paste", without changing the clipboard
+         hs.eventtap.keyStrokes(string) -- Defeating paste blocking http://www.hammerspoon.org/go/#pasteblock
+      else
+         pasteboard.setContents(string)
+         last_change = pasteboard.changeCount() -- Updates last_change to prevent item duplication when putting on paste
+      end
+   end
+end
+
+-- Clears the clipboard and history
+function clearAll()
+   pasteboard.clearContents()
+   clipboard_history = {}
+   settings.set("so.victor.hs.jumpcut",clipboard_history)
+   now = pasteboard.changeCount()
+   setTitle()
+end
+
+-- Clears the last added to the history
+function clearLastItem()
+   table.remove(clipboard_history,#clipboard_history)
+   settings.set("so.victor.hs.jumpcut",clipboard_history)
+   now = pasteboard.changeCount()
+   setTitle()
+end
+
+function pasteboardToClipboard(item)
+   -- Loop to enforce limit on qty of elements in history. Removes the oldest items
+   while (#clipboard_history >= hist_size) do
+      table.remove(clipboard_history,1)
+   end
+   table.insert(clipboard_history, item)
+   settings.set("so.victor.hs.jumpcut",clipboard_history) -- updates the saved history
+   setTitle() -- updates the menu counter
+end
+
+-- Dynamic menu by cmsj https://github.com/Hammerspoon/hammerspoon/issues/61#issuecomment-64826257
+populateMenu = function(key)
+   setTitle() -- Update the counter every time the menu is refreshed
+   menuData = {}
+   if (#clipboard_history == 0) then
+      table.insert(menuData, {title="None", disabled = true}) -- If the history is empty, display "None"
+   else
+      for k,v in pairs(clipboard_history) do
+         if (string.len(v) > label_length) then
+            table.insert(menuData,1, {title=string.sub(v,0,label_length).."…", fn = function() putOnPaste(v,key) end }) -- Truncate long strings
+         else
+            table.insert(menuData,1, {title=v, fn = function() putOnPaste(v,key) end })
+         end -- end if else
+      end-- end for
+   end-- end if else
+   -- footer
+   table.insert(menuData, {title="-"})
+   table.insert(menuData, {title="Clear All", fn = function() clearAll() end })
+   if (key.alt == true or pasteOnSelect) then
+      table.insert(menuData, {title="Direct Paste Mode ✍", disabled=true})
+   end
+   return menuData
+end
+
+-- If the pasteboard owner has changed, we add the current item to our history and update the counter.
+function storeCopy()
+   now = pasteboard.changeCount()
+   if (now > last_change) then
+      current_clipboard = pasteboard.getContents()
+      -- asmagill requested this feature. It prevents the history from keeping items removed by password managers
+      if (current_clipboard == nil and honor_clearcontent) then
+         clearLastItem()
+      else
+         pasteboardToClipboard(current_clipboard)
+      end
+      last_change = now
+   end
+end
+
+--Checks for changes on the pasteboard. Is it possible to replace with eventtap?
+timer = hs.timer.new(frequency, storeCopy)
+timer:start()
+
+setTitle() --Avoid wrong title if the user already has something on his saved history
+jumpcut:setMenu(populateMenu)
+
+hs.hotkey.bind({"cmd", "shift"}, "v", function() jumpcut:popupMenu(hs.mouse.getAbsolutePosition()) end)
---
+++ modules/window.lua
@@ -0,0 +1,144 @@
+-- Window management
+
+-- Defines for window maximize toggler
+local frameCache = {}
+local logger = hs.logger.new("windows")
+
+-- Resize current window
+
+function winresize(how)
+   local win = hs.window.focusedWindow()
+   local app = win:application():name()
+   local windowLayout
+   local newrect
+
+   if how == "left" then
+      newrect = hs.layout.left50
+   elseif how == "right" then
+      newrect = hs.layout.right50
+   elseif how == "up" then
+      newrect = {0,0,1,0.5}
+   elseif how == "down" then
+      newrect = {0,0.5,1,0.5}
+   elseif how == "max" then
+      newrect = hs.layout.maximized
+   elseif how == "left_third" or how == "hthird-0" then
+      newrect = {0,0,1/3,1}
+   elseif how == "middle_third_h" or how == "hthird-1" then
+      newrect = {1/3,0,1/3,1}
+   elseif how == "right_third" or how == "hthird-2" then
+      newrect = {2/3,0,1/3,1}
+   elseif how == "top_third" or how == "vthird-0" then
+      newrect = {0,0,1,1/3}
+   elseif how == "middle_third_v" or how == "vthird-1" then
+      newrect = {0,1/3,1,1/3}
+   elseif how == "bottom_third" or how == "vthird-2" then
+      newrect = {0,2/3,1,1/3}
+   end
+
+   win:move(newrect)
+end
+
+function winmovescreen(how)
+   local win = hs.window.focusedWindow()
+   if how == "left" then
+      win:moveOneScreenWest()
+   elseif how == "right" then
+      win:moveOneScreenEast()
+   end
+end
+
+-- Toggle a window between its normal size, and being maximized
+function toggle_window_maximized()
+   local win = hs.window.focusedWindow()
+   if frameCache[win:id()] then
+      win:setFrame(frameCache[win:id()])
+      frameCache[win:id()] = nil
+   else
+      frameCache[win:id()] = win:frame()
+      win:maximize()
+   end
+end
+
+-- Move between thirds of the screen
+function get_horizontal_third(win)
+   local frame=win:frame()
+   local screenframe=win:screen():frame()
+   local relframe=hs.geometry(frame.x-screenframe.x, frame.y-screenframe.y, frame.w, frame.h)
+   local third = math.floor(3.01*relframe.x/screenframe.w)
+   logger.df("Screen frame: %s", screenframe)
+   logger.df("Window frame: %s, relframe %s is in horizontal third #%d", frame, relframe, third)
+   return third
+end
+
+function get_vertical_third(win)
+   local frame=win:frame()
+   local screenframe=win:screen():frame()
+   local relframe=hs.geometry(frame.x-screenframe.x, frame.y-screenframe.y, frame.w, frame.h)
+   local third = math.floor(3.01*relframe.y/screenframe.h)
+   logger.df("Screen frame: %s", screenframe)
+   logger.df("Window frame: %s, relframe %s is in vertical third #%d", frame, relframe, third)
+   return third
+end
+
+function left_third()
+   local win = hs.window.focusedWindow()
+   local third = get_horizontal_third(win)
+   if third == 0 then
+      winresize("hthird-0")
+   else
+      winresize("hthird-" .. (third-1))
+   end
+end
+
+function right_third()
+   local win = hs.window.focusedWindow()
+   local third = get_horizontal_third(win)
+   if third == 2 then
+      winresize("hthird-2")
+   else
+      winresize("hthird-" .. (third+1))
+   end
+end
+
+function up_third()
+   local win = hs.window.focusedWindow()
+   local third = get_vertical_third(win)
+   if third == 0 then
+      winresize("vthird-0")
+   else
+      winresize("vthird-" .. (third-1))
+   end
+end
+
+function down_third()
+   local win = hs.window.focusedWindow()
+   local third = get_vertical_third(win)
+   if third == 2 then
+      winresize("vthird-2")
+   else
+      winresize("vthird-" .. (third+1))
+   end
+end
+
+-------- Key bindings
+
+-- Halves of the screen
+hs.hotkey.bind({"ctrl",        "cmd"}, "Left",  hs.fnutils.partial(winresize, "left"))
+hs.hotkey.bind({"ctrl",        "cmd"}, "Right", hs.fnutils.partial(winresize, "right"))
+hs.hotkey.bind({"ctrl",        "cmd"}, "Up",    hs.fnutils.partial(winresize, "up"))
+hs.hotkey.bind({"ctrl",        "cmd"}, "Down",  hs.fnutils.partial(winresize, "down"))
+
+-- Thirds of the screen
+hs.hotkey.bind({"ctrl", "alt"       }, "Left",  left_third)
+hs.hotkey.bind({"ctrl", "alt"       }, "Right", right_third)
+hs.hotkey.bind({"ctrl", "alt"       }, "Up",    up_third)
+hs.hotkey.bind({"ctrl", "alt"       }, "Down",  down_third)
+
+-- Maximized
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "F",     hs.fnutils.partial(winresize, "max"))
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "Up",    hs.fnutils.partial(winresize, "max"))
+
+-- Move between screens
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "Left",  hs.fnutils.partial(winmovescreen, "left"))
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "Right", hs.fnutils.partial(winmovescreen, "right"))
