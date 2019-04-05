8be9e6e244fd5088e201c13ae6976b40c8a7cccd
wangshub <rocksong.hit@gmail.com>
2019-04-05 21:39:53 +0800

    feat(ui): weather update menubar data item

--- modules/weather.lua
+++ modules/weather.lua
@@ -1,4 +1,6 @@
+local urlApi = 'https://www.tianqiapi.com/api/?version=v1'
 local menubar = hs.menubar.new()
+local menuData = {}
 
 menubar:setTooltip("Weather Info")
 menubar:setTitle('🌤')
@@ -17,11 +19,26 @@ end
 
 menubar:setMenu(popupMenu)
 
+function updateMenubar()
+   menubar:setMenu(menuData)
+end
 
 function getWeather()
    print('today is a good day.')
+   code, body, htable = hs.http.get(urlApi, nil)
+   if code ~= 200 then
+      print('get weather error:'..code)
+      return
+   end
+   rawjson = hs.json.decode(body)
+   menuData = {}
+   for k, v in pairs(rawjson.data) do
+      item = { title = v.day }
+      table.insert(menuData, item)
+   end
+   updateMenubar()
 end
 
 
-timer = hs.timer.new(6, getWeather)
+timer = hs.timer.new(60, getWeather)
 timer:start()
